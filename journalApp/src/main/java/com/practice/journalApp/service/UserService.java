db059c4f7991ff9f9d436f8de7c81438fffecd7d
Naman Sharma <naman99sha@gmail.com>
2024-10-04 12:48:52 +0530

    Completed User Service

--- journalApp/src/main/java/com/practice/journalApp/service/UserService.java
+++ journalApp/src/main/java/com/practice/journalApp/service/UserService.java
@@ -1,7 +1,7 @@
 package com.practice.journalApp.service;
 
-import com.practice.journalApp.entity.JournalEntry;
-import com.practice.journalApp.repository.JournalEntryRepo;
+import com.practice.journalApp.entity.User;
+import com.practice.journalApp.repository.UserRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,5 +11,26 @@ import java.util.Optional;
 
 @Component
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
 
+    public void saveEntry(User user){
+        userRepo.save(user);
+    }
+
+    public List<User> getAll(){
+        return userRepo.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id){
+        return userRepo.findById(id);
+    }
+
+    public void deleteAll(){
+        userRepo.deleteAll();
+    }
+
+    public void deleteById(ObjectId id){
+        userRepo.deleteById(id);
+    }
 }
